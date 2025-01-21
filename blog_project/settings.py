fadf0afce4c9c36a2f963ea4d64e7b66ae4af441
Lenny Le'Mashon <leonardlemashon@gmail.com>
2025-01-21 22:00:10 +0300

    Heroku config files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-og2@yuc77b!bt#p$5o^(*rf4cm-a4-@ol_qr-7-ck7tfj5on6w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
