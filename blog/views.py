1db595cda53c64870278cadb99513cd1790038a7
Lenny Le'Mashon <leonardlemashon@gmail.com>
2025-01-21 14:38:00 +0300

    Adding templates

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -11,3 +11,9 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
